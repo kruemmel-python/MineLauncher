f89f1d900b9ccda6a319a593f121a31714b8406f
Ralf Krümmel <ralf.kruemmel+python@outlook.de>
2025-12-28 12:14:25 +0100

    Add behavior context state helpers

--- plugins/RPGPlugin/src/main/java/com/example/rpg/behavior/BehaviorNode.java
+++ plugins/RPGPlugin/src/main/java/com/example/rpg/behavior/BehaviorNode.java
@@ -4,9 +4,11 @@ import java.util.UUID;
 
 public abstract class BehaviorNode {
     private final String id;
+    protected final String nodeId;
 
     protected BehaviorNode(String id) {
         this.id = id;
+        this.nodeId = id;
     }
 
     public String id() {
